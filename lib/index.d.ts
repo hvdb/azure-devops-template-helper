447312b574d6479d38459db7df5e64bee77f38a3
Henk van den Brink <hendrik.van.den.brink@ing.com>
2021-08-12 15:03:57 +0200

    chore: add lib

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+declare const adoTemplateHelper: () => void;
+export default adoTemplateHelper;
---
+++ lib/snippets.d.ts
@@ -0,0 +1,7 @@
+/**
+ *
+ *
+ * @param jsonData Template json data
+ * @returns the snippet
+ */
+export declare function createSnippet(jsonTemplateData: any, prefix: string): string;
